87730bd31b6517a513d429dc6b6aa0b0c370a55c
Iris Van Melsen <ivan-mel@f0r5s18.codam.nl>
2022-11-08 17:28:34 +0100

    printf

---
+++ Makefile
@@ -0,0 +1,63 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ivan-mel <ivan-mel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/11/08 15:58:37 by ivan-mel          #+#    #+#              #
+#    Updated: 2022/11/08 17:03:35 by ivan-mel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME 		= 	libftprintf.a
+RM 			=	rm -rf
+FLAGS 		=	-Wall -Werror -Wextra
+CC 			= 	gcc
+AR 			=	ar -rc
+SRC			=	ft_printf.c \
+				printfunctions.c \
+				hexafunctions.c \
+#SRCB		=	
+OBJ_FILES	=	${SRC:.c=.o}
+OBJB_FILES	=	${SRCB:.c=.o}
+OBJ_DIR	=	./obj/
+# Reset
+Color_Off	=	"\033[0m"			# Text Reset
+# Regular Colors
+Black		=	"\033[0;30m"		# Black
+Red			=	"\033[0;31m"		# Red
+Green		=	"\033[0;32m"		# Green
+Yellow		=	"\033[0;33m"		# Yellow
+Blue		=	"\033[0;34m"		# Blue
+Purple		=	"\033[0;35m"		# Purple
+Cyan		=	"\033[0;36m"		# Cyan
+White		=	"\033[0;37m"		# White
+# Avoid relinking in bonus
+ifdef WITH_BONUS
+	OBJ_RAW = $(OBJ_FILES) $(OBJB_FILES)
+else
+	OBJ_RAW = $(OBJ_FILES)
+endif
+# Add obj directory to obj path
+OBJ = $(addprefix $(OBJ_DIR), $(OBJ_RAW))
+all:	${NAME}
+${NAME}: ${OBJ}
+	@echo ${Blue} Building ${NAME} ${Color_Off}
+	@${AR} ${NAME} $?
+	@echo ${Green} Complete 😊 ${Color_off}
+$(OBJ_DIR)%.o: %.c
+	@mkdir -p ${OBJ_DIR}
+	@echo ${Blue} Compiling: $< ${Color_Off}
+	@${CC} ${FLAGS} -c $< -o $@
+# Set 'WITH_BONUS' option to make with bonus
+#bonus:
+#	@$(MAKE) WITH_BONUS=true all
+clean:
+	@echo ${Yellow} Deleting ${OBJ_DIR} ${Color_off}
+	@${RM} ${OBJ_DIR}
+fclean: clean
+	@echo ${Yellow} Deleting ${NAME} ${Color_off}
+	@${RM} ${NAME}
+re:	fclean all
+.PHONY: all bonus clean fclean re
--- ft_printf.c
+++ ft_printf.c
@@ -6,117 +6,185 @@
 /*   By: ivan-mel <ivan-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 21:57:53 by iris              #+#    #+#             */
-/*   Updated: 2022/11/08 11:31:06 by ivan-mel         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:27:59 by ivan-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <string.h>
+#include "ft_printf.h"
 #include <stdio.h>
 #include <unistd.h>
 
-int printchar(char c)
-{
-    write(1, &c, 1);
-    return (1);
-}
+// int printchar(char c)
+// {
+//     write(1, &c, 1);
+//     return (1);
+// }
 
-int printstr(char *str)
-{
-    int i;
-    int counter;
-    
-    i = 0;
-    counter = 0;
-    if (str == NULL)
-        return (write(1, "(null)", 6));
-    while (str[i] != '\0')
-    {
-        counter += printchar(str[i]);
-        i++;
-    }
-    return (counter);
-}
+// int printstr(char *str)
+// {
+//     int i;
+//     int counter;
 
-int	putnbr(int nb)
-{
-	char	a;
+//     i = 0;
+//     counter = 0;
+//     if (str == NULL)
+//         return (write(1, "(null)", 6));
+//     while (str[i] != '\0')
+//     {
+//         counter += printchar(str[i]);
+//         i++;
+//     }
+//     return (counter);
+// }
 
-	if (nb == -2147483648)
-		write (1, "-2147483648", 11);
-	if (nb < 0 && nb != -2147483648)
+// int	putnbr(int nb)
+// {
+//     int     counter;
+
+//     counter = 0;
+// 	if (nb == -2147483648)
+//         return (write(1, "-2147483648", 11));
+//     if (nb < 0)
+// 	{
+//         nb = nb * -1;
+// 		counter += printchar('-');
+// 	}
+// 	if (nb < 10)
+//     {
+// 		counter += printchar(nb + '0');
+// 	} 
+//     if (nb >= 10)
+//     {
+// 		counter += putnbr(nb / 10);
+// 		counter += putnbr(nb % 10);
+//     }
+//     return (counter);
+// }
+
+// int	putun(unsigned int nb)
+// {
+//     unsigned int     counter;
+
+//     counter = 0;
+// 	if (nb < 10)
+//     {
+// 		counter += printchar(nb + '0');
+// 	} 
+//     if (nb >= 10)
+//     {
+// 		counter += putnbr(nb / 10);
+// 		counter += putnbr(nb % 10);
+//     }
+//     return (counter);
+// }
+
+// int printhexalow(unsigned long nb, char const *str)
+// {
+//     unsigned int     counter;
+
+//     counter = 0;
+// 	if (nb < 16)
+//     {
+// 		counter += printchar(str[nb]);
+// 	} 
+//     if (nb >= 16)
+//     {
+// 		counter += printhexalow(nb / 16, "0123456789abcdef");
+// 		counter += printhexalow(nb % 16, "0123456789abcdef");
+//     }
+//     return (counter);
+// }
+
+// int printhexaup(unsigned long nb, char const *str)
+// {
+//     unsigned int     counter;
+
+//     counter = 0;
+// 	if (nb < 16)
+//     {
+// 		counter += printchar(str[nb]);
+// 	} 
+//     if (nb >= 16)
+//     {
+// 		counter += printhexaup(nb / 16, "0123456789ABCDEF");
+// 		counter += printhexaup(nb % 16, "0123456789ABCDEF");
+//     }
+//     return (counter);
+// }
+
+int	formatlist(va_list args, char format)
+{
+	int	result;
+
+	result = 0;
+	if (format == 'c')
+		result = printchar(va_arg(args, int));
+	else if (format == '%')
+		result = printchar('%');
+	else if (format == 's')
+		result = printstr(va_arg(args, char *));
+	else if (format == 'd' || format == 'i')
+		result = putnbr(va_arg(args, int));
+	else if (format == 'u')
+		result = putun(va_arg(args, unsigned int));
+	else if (format == 'x')
+		result = printhexalow(va_arg(args, unsigned int), "0123456789abcdef");
+	else if (format == 'X')
+		result = printhexaup(va_arg(args, unsigned int), "0123456789ABCDEF");
+	else if (format == 'p')
 	{
-		printchar('-');
-		nb = nb * -1;
+		write(1, "0x", 2);
+		result = printhexalow(va_arg(args, unsigned long), "0123456789abcdef");
+		result += 2;
 	}
-	if (nb < 10 && nb != -2147483648)
+	return (result);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		i;
+	int		result;
+	va_list	args;
+
+	i = 0;
+	result = 0;
+	va_start(args, format);
+	while (format[i] != '\0')
 	{
-		a = nb + '0';
-		printchar(a);
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			result += formatlist(args, format[i]);
+		}
+		else if (format [i] != '%')
+			result += printchar(format[i]);
+		i++;
 	}
-	if (nb >= 10)
-	{
-		putnbr(nb / 10);
-		putnbr(nb % 10);
-	} 
-    return (nb);
+	va_end(args);
+	return (result);
 }
 
-int formatlist(va_list args, char format)
-{
-    int result;
+// int main(void)
+// {
+//     // int a;
+//     // int b;
 
-    result = 0;
-    if (format == 'c')
-        result = printchar(va_arg(args, int));
-    else if (format == '%')
-        result = printchar('%');
-    else if (format == 's')
-        result = printstr(va_arg(args, char *));
-    else if (format == 'd' || format == 'i')
-        result = putnbr(va_arg(args, int));
- return (result);
-}
-
-int ft_printf(const char *format, ...)
-{
-    int i;
-    int result;
-    va_list args;
-
-    i = 0;
-    result = 0;
-    va_start(args, format);
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            result += formatlist(args, format[i]);
-        }
-        else
-            result += printchar(format[i]);
-        i++;
-    }
-    va_end(args);
-    return (result);
-}
-
-int main(void)
-{
-    int a;
-    int b;
-
-    // printf("printf: He %c l %c lo\n", '9', '8');
-    // ft_printf("ft_printf: He %c l %c lo\n", '9', '8');
-    // printf("printf: He l %% w l l o \n");
-    // ft_printf("ft_printf: He %%  l l o \n");
-    // printf("printf: He l %s l l o \n", "Hello");
-    // ft_printf("ft_printf: He %s l l o \n", NULL);
-    // printf("printf: He %i l l o \n", 8);
-    // ft_printf("ft_printf: He %i l l o \n", 8);
-    a = printf("printf: He %i l l o \n", 9);
-    b = ft_printf("ft_printf: He %i l l o \n", 9);
-    printf("%i %i", a, b);
-    // printf("%s", NULL);
-}
+// //     // printf("printf: He %c l %c lo\n", '9', '8');
+// //     // ft_printf("ft_printf: He %c l %c lo\n", '9', '8');
+// //     // printf("printf: He l %% w l l o \n");
+// //     // ft_printf("ft_printf: He %%  l l o \n");
+// //     // printf("printf: He l %s l l o \n", "Hello");
+// //     // ft_printf("ft_printf: He %s l l o \n", NULL);
+// //     // printf("printf: He %i l l o \n", 8);
+// //     // ft_printf("ft_printf: He %i l l o \n", 8);
+// //     //a = printf("ft: He %li l l o \n", -2147483648);
+// //     //b = ft_printf("lb: He %i l l o \n", -2147483648);
+// //     //a = printf("ft: He %u l l o \n", 0);
+// //     //b = ft_printf("lb: He %u l l o \n", 0);
+//     //  a = printf("%x", 123);
+//     //  b = ft_printf("%x\n", 123);
+//     printf("%d", ft_printf("%%%\0fjsfjklsdfjls"));
+// //     // printf("%s", NULL);
+// }
